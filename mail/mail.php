bbd86428b84035657f47cfa1f9ad4e0c968232ad
alexchaika1987 <seegalprometey@gmail.com>
2019-04-23 18:28:41 +0300

    Revert "init commit"
    This reverts commit e63c8158d3594819450e8172ffc0f208d9eb72e6.

--- mail/mail.php
+++ mail/mail.php
@@ -1,38 +1,4 @@
-// Routes
-
-$api->get('projects', 'App\Api\V1\Controllers\ProjectController@index');
-$api->post('projects', 'App\Api\V1\Controllers\ProjectController@store');
-
-// Controller
-
 <?php
-
-namespace App\Api\V1\Controllers;
-use App\Api\V1\Models\Project;
-use App\Api\V1\Controllers\BaseController;
-use Dingo\Api\Http\Request;
-
-class ProjectController extends BaseController
-{
-    public function index(Request $request)
-    {
-         echo 'index';
-    }
-
-    
-    public function show($id)
-    {
-        $project = Project::findOrFail($id);
-        return $this->response->array($project->toArray());
-    }
-
-    public function store(Request $request)
-    {
-       echo 'stored';
-    }
-}
-   
-
 
 $recepient = "alexchaika1987@gmail.com";
 $siteName = "bestproperty.ml";
@@ -44,3 +10,4 @@ $message = "Имя: $name \nТелефон: $phone";
 $pagetitle = "Заявка с сайта \"$siteName\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
 
+?>
